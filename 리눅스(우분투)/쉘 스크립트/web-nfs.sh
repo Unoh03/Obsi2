e984e8a91c14e6e7313990415dbf821b79ca740c
Unoh03 <unoh0626@gmail.com>
2026-04-07 11:14:47 +0900

    Auto: 2026. 04. 07. 11:14:47 M 리눅스(우분투)/쉘 스크립트/netplan.txt 리눅스(우분투)/쉘 스크립트/web-nfs.sh

--- 리눅스(우분투)/쉘 스크립트/web-nfs.sh
+++ 리눅스(우분투)/쉘 스크립트/web-nfs.sh
@@ -3,7 +3,7 @@ echo "[INFO] NFS 클라이언트 세팅을 시작합니다..."
 sudo apt update
 sudo apt install -y nfs-common
 mkdir ~/share_client
-echo '192.168.3.4:/share_directory /home/kyes-user/share_client nfs defaults 0 0' | sudo tee -a /etc/fstab
+echo '192.168.3.4:/share_directory /home/nfs/share_client nfs defaults 0 0' | sudo tee -a /etc/fstab
 # NFS 서버(192.168.3.4)가 어떤 폴더를 공유(Export)하고 있는지 확인
 showmount -e 192.168.3.4
 echo "[SUCCESS] NFS 클라이언트 세팅이 완벽하게 끝났습니다!"
