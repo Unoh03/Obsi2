fec57381e39ff9092177869974bee9cdaac1d9a4
Unoh03 <unoh0626@gmail.com>
2026-04-15 11:45:17 +0900

    Auto: 2026. 04. 15. 11:45:17 M .obsidian/workspace.json 스프링 툴/자바스크립트/JsController.java 스프링 툴/자바스크립트/예제/ex02.jsp 스프링 툴/자바스크립트/자바 스크립트.md, A 스프링 툴/자바스크립트/예제/ex01New.jsp 스프링 툴/자바스크립트/예제/ex03.jsp 스프링 툴/자바스크립트/예제/ex04.jsp 스프링 툴/자바스크립트/예제/ex05.jsp 스프링 툴/자바스크립트/예제/ex06.jsp 스프링 툴/자바스크립트/예제/ex07.jsp 스프링 툴/자바스크립트/예제/ex08.jsp 스프링 툴/자바스크립트/예제/ex09.jsp 스프링 툴/자바스크립트/예제/ex10.jsp

--- 스프링 툴/자바스크립트/JsController.java
+++ 스프링 툴/자바스크립트/JsController.java
@@ -9,7 +9,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class JsController {
 	@RequestMapping("ex01")
 	public void ex01() {}
+    @RequestMapping("ex01New")
+	public void ex01New() {}
     @RequestMapping("ex02")
 	public void ex02() {}
-	
+    @RequestMapping("ex03")
+	public void ex03() {}
+    @RequestMapping("ex04")
+	public void ex04() {}
+    @RequestMapping("ex05")
+	public void ex05() {}
+    @RequestMapping("ex06")
+	public void ex06() {}
+    @RequestMapping("ex07")
+	public void ex07() {}
+    @RequestMapping("ex08")
+	public void ex08() {}
+    @RequestMapping("ex09")
+	public void ex09() {}
+    @RequestMapping("ex10")
+	public void ex10() {}
 }
